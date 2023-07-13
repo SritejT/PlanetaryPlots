d68b32047417519dec5c315bc295f75fac176f57
Sritej Tummuru <sritej.tummuru@gmail.com>
2023-07-13 11:19:01 +0100

    Initial Commit

---
+++ PlanetaryPlots/PlanetaryPlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetaryPlotsApp.swift
+//  PlanetaryPlots
+//
+//  Created by Ravindra Tummuru on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanetaryPlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
