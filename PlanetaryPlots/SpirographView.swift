9648616a45e393b237e61eae31fdc6bae043567b
Sritej Tummuru <sritej.tummuru@gmail.com>
2023-07-21 16:17:13 +0100

    Improved usability and clarity of UI for spirograph tab.

--- PlanetaryPlots/SpirographView.swift
+++ PlanetaryPlots/SpirographView.swift
@@ -14,80 +14,115 @@ struct SpirographView: View {
         var id: Self { self }
     }
     
-    
 
-    @State var planet1: Planets = .Mercury
-    @State var planet2: Planets = .Mercury
+    @State private var planet1: Planets = .Mercury
+    @State private var planet2: Planets = .Mercury
     @State private var isLoading = false
+    @State private var loading_id = 0
     
-    
-    
+    @State private var loading_pos = -110.0
     @StateObject var state = SpirographStateController()
     
     var body: some View {
-        VStack(spacing: 50) {
-            HStack(spacing: 10) {
-                Text("Select 1st planet for spirograph:")
-                Picker("Select 1st planet for spirograph", selection: $planet1) {
-                    ForEach(Planets.allCases) { planet in
-                        Text(planet.rawValue)
+        ScrollView {
+            VStack(spacing: 30) {
+                Spacer()
+                    .frame(height: 30)
+                HStack(spacing: 10) {
+                    Text("Select 1st planet for spirograph:")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Picker("Select 1st planet for spirograph", selection: $planet1) {
+                        ForEach(Planets.allCases) { planet in
+                            Text(planet.rawValue)
+                        }
                     }
+                    .accentColor(.white)
+                }
+                .padding()
+                .background(
+                    Color.blue
+                        .cornerRadius(10)
+                )
+                
+                HStack(spacing: 10) {
+                    Text("Select 2nd planet for spirograph:")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    Picker("Select 2nd planet for spirograph", selection: $planet2) {
+                        ForEach(Planets.allCases) { planet in
+                            Text(planet.rawValue)
+                        }
+                    }
+                    .accentColor(.white)
+                }
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(10)
+                
+                Spacer()
+                    .frame(height: 0)
+                
+                Button(action: {
+                    isLoading.toggle()
+                    state.getSpirograph(planet1: planet1.rawValue, planet2: planet2.rawValue)
+                    loading_id += 1
+                }, label: {
+                    Text("Generate Spirograph")
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .padding(.horizontal, 20)
+                        .background(
+                            Color(red: 0.0, green: 0.1, blue: 1.0)
+                                .cornerRadius(10)
+                                .shadow(radius: 10)
+                        )
+                })
+                
+                Divider()
+                
+                if state.isLoading {
+                    Spacer()
+                        .frame(height: 160)
+                    ZStack {
+                        Text("Loading...")
+                            .font(.system(.body, design: .rounded))
+                            .bold()
+                            .offset(x: 0, y: -25)
+             
+                        RoundedRectangle(cornerRadius: 3)
+                            .stroke(Color(.systemGray5), lineWidth: 3)
+                            .frame(width: 250, height: 3)
+             
+                        RoundedRectangle(cornerRadius: 3)
+                            .stroke(Color.blue, lineWidth: 3)
+                            .frame(width: 30, height: 3)
+                            .offset(x: loading_pos, y: 0)
+
+                    }
+                    .onAppear {
+                        loading_pos = -110
+                        withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+                            loading_pos = 110
+                        }
+                    }
+                    
+                } else if state.encodedImage != "" {
+                    let decodedImage = Data(base64Encoded: state.encodedImage) ?? Data()
+                    let image = (UIImage(data: decodedImage) ?? UIImage(systemName: "questionmark.folder.fill"))
+                
+                    Image(uiImage: image!)
+                        .resizable()
+                        .frame(width: 400.0, height: 400.0)
+                        .scaledToFit()
                 }
             }
-            HStack(spacing: 10) {
-                Text("Select 2nd planet for spirograph:")
-                Picker("Select 2nd planet for spirograph", selection: $planet2) {
-                    ForEach(Planets.allCases) { planet in
-                        Text(planet.rawValue)
-                    }
-                }
-            }
-            Button(action: {
-                isLoading.toggle()
-                state.getSpirograph(planet1: planet1.rawValue, planet2: planet2.rawValue)
-            }, label: {
-                Text("Generate Spirograph".uppercased())
-                    .font(.headline)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.white)
-                    .padding()
-                    .padding(.horizontal, 20)
-                    .background(
-                        Color.blue
-                            .cornerRadius(10)
-                            .shadow(radius: 10)
-                    )
-            })
-            
-            ZStack {
-                Text("Loading")
-                    .font(.system(.body, design: .rounded))
-                    .bold()
-                    .offset(x: 0, y: -25)
-     
-                RoundedRectangle(cornerRadius: 3)
-                    .stroke(Color(.systemGray5), lineWidth: 3)
-                    .frame(width: 250, height: 3)
-     
-                RoundedRectangle(cornerRadius: 3)
-                    .stroke(Color.blue, lineWidth: 3)
-                    .frame(width: 30, height: 3)
-                    .offset(x: state.isLoading ? 110 : -110, y: 0)
-                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: true))
-            }
-            .opacity(state.isLoading == true ? 1.0 : 0.0)
-            
-            let decodedImage = Data(base64Encoded: state.encodedImage) ?? Data()
-            let image = (UIImage(data: decodedImage) ?? UIImage(systemName: "questionmark.folder.fill"))
-        
-            Image(uiImage: image!)
-                .resizable()
-                .frame(width: 400.0, height: 400.0)
-                .scaledToFit()
-            
-            
+            .pickerStyle(.menu)
         }
-        .pickerStyle(.menu)
     }
 }
 
